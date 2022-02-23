bc91bb99b6c12405ef908e664ff90654c6b0796d
Janak Damre <janaksd2000@gmial.com>
2022-02-24 00:02:10 +0530

    Todos-List_ReactJS

---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error nam blanditiis labore hic magnam, consequatur animi nulla nobis reprehenderit odio esse accusantium totam doloribus perspiciatis at veniam perferendis laudantium saepe minima et quo! Fuga.</p>
+        </div>
+    )
+}
+
+export default About
